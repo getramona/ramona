db7152cbac8c25aea68b3a5fadd39142d59e2bcd
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-03-07 00:20:58 -0500

    fix user team associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ActiveRecord::Base
 
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships, source: :group, source_type: 'Organization'
-  has_many :organizations, through: :memberships, source: :group, source_type: 'Team'
+  has_many :teams, through: :memberships, source: :group, source_type: 'Team'
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
