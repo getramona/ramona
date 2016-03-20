246c2c157185c7224914dc6face2133df074c206
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-03-20 15:38:28 -0400

    don't require unique name for organization.

--- spec/app/models/organization_spec.rb
+++ spec/app/models/organization_spec.rb
@@ -7,10 +7,6 @@ describe Organization, type: :model do
     expect(organization).to validate_presence_of :name
   end
 
-  it 'has a unique name' do
-    expect(organization).to validate_uniqueness_of :name
-  end
-
   it 'has a subdomain' do
     expect(organization).to validate_presence_of :subdomain
   end
