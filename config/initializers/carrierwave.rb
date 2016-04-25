2e4ed236615913247dc438bf45393dd3121ce122
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-04-25 01:25:54 -0400

    fix upload issues (s3 region).

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,9 +18,14 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    aws
   }
 
   # Change the AWS bucket
-  config.fog_directory = "ramona-#{Rails.env}"
+  if Rails.env == 'production'
+    config.fog_directory = "ramona-production-2"
+  else
+    config.fog_directory = "ramona-#{Rails.env}"
+  end
 end
